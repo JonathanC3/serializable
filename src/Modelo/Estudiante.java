6d6ea4fee96f1c36c6954bf03e29837051ac3bc5
jonathan <jonathan@192.168.1.6>
2018-03-28 14:02:21 -0600

    ejemplo serializable

---
+++ src/Modelo/Estudiante.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author jonathan
+ */
+public class Estudiante implements Serializable{
+    
+    private static final long serialVersionUID= -1L;
+    
+    private String id;
+    private String nombre;
+    private String apellido;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+    
+    
+}
---
+++ src/objetoserializable/ObjetoSerializable.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetoserializable;
+import gui.frmEstudiante;
+/**
+ *
+ * @author jonathan
+ */
+public class ObjetoSerializable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmEstudiante es= new frmEstudiante();
+        es.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
